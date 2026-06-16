8ddb2acb54cb35597b70cecc8ce19260295026c6
Vitech <i@vitechliu.com>
2026-06-16 23:28:40 +0800

    feat: 连接召唤part2

--- Core/Effects/NLinkSummonManager.cs
+++ Core/Effects/NLinkSummonManager.cs
@@ -8,29 +8,54 @@ namespace VYgo.Core.Effects;
 public partial class NLinkSummonManager: Node3D {
 	private List<NLinkTrailManager> linkTrails = new();
 
+	private Sprite3D _shineSquare;
+
+	private Tween? _shineTween;
+
+	private AnimationPlayer _lineAnimationPlayer;
+
 	public override void _Ready() {
 		base._Ready();
+		_shineSquare = GetNode<Sprite3D>("%GateSquare2");
+		_lineAnimationPlayer = GetNode<AnimationPlayer>("PostLinkFX/LineEffect/LineEffectPlayer");
 		for (var i = 1; i <= 8; i++) {
 			var node = GetNode<NLinkTrailManager>((NodePath) "%LinkTrail" + i);
 			if (node == null) {
 				Entry.Logger.Warn("CannotFindLinkTrail:" + i);
 				return;
 			}
-
+			node._parent = this;
 			linkTrails.Add(node);
 		}
 	}
 
+	public void ShineSquare(float time) {
+		_shineSquare.Modulate = _shineSquare.Modulate with { A = 1f };
+		if (_shineTween != null) {_shineTween.Kill();}
+		_shineTween = CreateTween();
+		_shineTween.TweenProperty(_shineSquare, "modulate:a", 0f, time)
+			.SetEase(Tween.EaseType.Out).SetTrans(Tween.TransitionType.Cubic);
+		// GD.Print("ShineSquare");
+	}
 	public void PlayLink() {
 		TaskHelper.RunSafely(play());
 	}
-	public async Task play() {
+	public void onHit() {
+		ShineSquare(1f);
+	}
+	async Task play() {
 		foreach (var trails in linkTrails) {
 			trails.SetVisible(true);
 			trails.play();
-			await Cmd.Wait(1f);
-			trails.SetVisible(false);
-			trails.QueueFreeSafely();
+			await Cmd.Wait(.3f);
+			// trails.SetVisible(false);
+			// trails.QueueFreeSafely();
 		}
+		await Cmd.Wait(.6f);
+		PlayPostEffect();
+	}
+
+	public void PlayPostEffect() {
+		_lineAnimationPlayer.Play("line");
 	}
 }
--- Core/Effects/NLinkTrailManager.cs
+++ Core/Effects/NLinkTrailManager.cs
@@ -5,6 +5,8 @@ namespace VYgo.Core.Effects;
 public partial class NLinkTrailManager: Node3D {
 	private AnimationPlayer _animationPlayer;
 
+	public NLinkSummonManager? _parent = null!;
+
 	public override void _Ready() {
 		_animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
 		var meshInstance = GetNode<MeshInstance3D>("MeshInstance3D");
@@ -15,5 +17,11 @@ public partial class NLinkTrailManager: Node3D {
 
 	public void play() {
 		_animationPlayer.Play("link_trail");
+	}
+
+	public void hit() {
+		if (_parent != null) {
+			_parent.onHit();
+		}
 	}
 }
