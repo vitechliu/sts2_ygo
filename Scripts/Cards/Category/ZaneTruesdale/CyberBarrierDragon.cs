7c5312af49e8e7342c59bc4f700186111018883a
Vitech <i@vitechliu.com>
2026-07-15 19:05:39 +0800

    feat: 电子障壁龙

---
+++ Scripts/Cards/Category/ZaneTruesdale/CyberBarrierDragon.cs
@@ -0,0 +1,23 @@
+using STS2RitsuLib.Interop.AutoRegistration;
+using VYgo.Scripts.Pools;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using VYgo.Core;
+
+namespace VYgo.Scripts.Cards.Category.ZaneTruesdale;
+
+[RegisterCard(typeof(ZaneTruesdaleCardPool))]
+public class CyberBarrierDragon() : BaseMonsterCard(2, CardRarity.Basic, TargetType.None) {
+    public override int CardId => 68774379;
+
+    public override int BaseAttackVar => 0;
+    public override int BaseLifeVar => 7;
+    public override int UpgradeAttackVar => 0;
+    public override int UpgradeLifeVar => 2;
+
+    // protected override IEnumerable<IHoverTip> AdditionalHoverTips => [
+    //     BaseSummonHoverTip,
+    // ];
+    
+    public override List<YgoArchetypes> ArchetypesList => [YgoArchetypes.Cyber];
+    
+}
---
+++ Scripts/Monsters/YGO/CyberBarrierDragonMinion.cs
@@ -0,0 +1,5 @@
+namespace VYgo.Scripts.Monsters.YGO;
+
+public class CyberBarrierDragonMinion: BaseMonster {
+    public override int CardId => 68774379;
+}
