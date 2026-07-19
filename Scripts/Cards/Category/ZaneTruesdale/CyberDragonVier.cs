1f7119bdbd489784e49e3cec1869e7184d3626cd
Vitech <i@vitechliu.com>
2026-07-20 02:09:42 +0800

    feat: 电子龙四型
    修复前面卡牌的一些问题

---
+++ Scripts/Cards/Category/ZaneTruesdale/CyberDragonVier.cs
@@ -0,0 +1,24 @@
+using STS2RitsuLib.Interop.AutoRegistration;
+using VYgo.Scripts.Pools;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using VYgo.Core;
+using VYgo.Scripts.Var;
+
+namespace VYgo.Scripts.Cards.Category.ZaneTruesdale;
+
+[RegisterCard(typeof(ZaneTruesdaleCardPool))]
+public class CyberDragonVier() : BaseMonsterCard(1, CardRarity.Rare, TargetType.None) {
+    public override int CardId => 29975188;
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new AttackVar(BaseAttackVar),
+        new LifeVar(BaseLifeVar),
+        new EnergyVar("CostReduction", 1)
+    ];
+
+    public override int BaseAttackVar => 3;
+    public override int BaseLifeVar => 4;
+    public override int UpgradeAttackVar => 1;
+    public override int UpgradeLifeVar => 1;
+}
