11f15cddf593f164bebb8a94ddad1903dd304f4f
Vitech <i@vitechliu.com>
2026-07-19 18:03:01 +0800

    optimize: 电子终结龙，优化文本

--- Scripts/Cards/Category/ZaneTruesdale/CyberBarrierDragon.cs
+++ Scripts/Cards/Category/ZaneTruesdale/CyberBarrierDragon.cs
@@ -2,20 +2,17 @@ using STS2RitsuLib.Interop.AutoRegistration;
 using VYgo.Scripts.Pools;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using VYgo.Core;
+using VYgo.Scripts.Characters;
 
 namespace VYgo.Scripts.Cards.Category.ZaneTruesdale;
 
 [RegisterCard(typeof(ZaneTruesdaleCardPool))]
-public class CyberBarrierDragon() : BaseMonsterCard(2, CardRarity.Basic, TargetType.None) {
+[RegisterCharacterStarterCard(typeof(ZaneTruesdaleCharacter), 2)]
+public class CyberBarrierDragon() : BaseMonsterCard(1, CardRarity.Basic, TargetType.None) {
     public override int CardId => 68774379;
 
     public override int BaseAttackVar => 0;
-    public override int BaseLifeVar => 7;
+    public override int BaseLifeVar => 5;
     public override int UpgradeAttackVar => 0;
     public override int UpgradeLifeVar => 2;
-
-    // protected override IEnumerable<IHoverTip> AdditionalHoverTips => [
-    //     BaseSummonHoverTip,
-    // ];
-    
 }
