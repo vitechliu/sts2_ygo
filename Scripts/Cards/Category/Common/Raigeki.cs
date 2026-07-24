1d062747682ab537b0efefee391a8f218dbf5c96
Vitech <i@vitechliu.com>
2026-07-24 22:17:15 +0800

    feat:雷击

---
+++ Scripts/Cards/Category/Common/Raigeki.cs
@@ -0,0 +1,30 @@
+using STS2RitsuLib.Interop.AutoRegistration;
+using VYgo.Scripts.Pools;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace VYgo.Scripts.Cards.Category.Common;
+
+[RegisterCard(typeof(CommonCardPool))]
+public class Raigeki() : BaseSpellCard(1, CardType.Skill, CardRarity.Common, TargetType.AllEnemies) {
+    public override int CardId => 12580477;
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new DamageVar(7m, ValueProp.Move)
+    ];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay) {
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue)
+            .FromCard(this, cardPlay)
+            .TargetingAllOpponents(CombatState)
+            .WithHitFx("vfx/vfx_attack_slash")
+            .Execute(choiceContext);
+    }
+
+    protected override void OnUpgrade() {
+        DynamicVars.Damage.UpgradeValueBy(2m);
+    }
+}
