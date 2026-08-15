a09f5e775e242f5d7fb37a67aa9c37cf46a832cb
Vitech <i@vitechliu.com>
2026-08-16 03:02:49 +0800

    feat: 电子革命系统

---
+++ Scripts/Cards/Category/ZaneTruesdale/CyberRevsystem.cs
@@ -0,0 +1,49 @@
+using MegaCrit.Sts2.Core.CardSelection;
+using MegaCrit.Sts2.Core.Commands;
+using STS2RitsuLib.Interop.AutoRegistration;
+using VYgo.Scripts.Pools;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Models;
+using VYgo.Core;
+using VYgo.Utils;
+
+namespace VYgo.Scripts.Cards.Category.ZaneTruesdale;
+
+[RegisterCard(typeof(ZaneTruesdaleCardPool))]
+public class CyberRevsystem() : BaseSpellCard(0, CardType.Skill, CardRarity.Common, TargetType.None) {
+    public override int CardId => 33041277;
+
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
+
+    protected override IEnumerable<IHoverTip> AdditionalHoverTips => [
+        YgoHoverTipConst.SpecialSummon()
+    ];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay) {
+        if (Owner.MinionCount() >= MinionUtil.MaxMinionCount) {
+            return;
+        }
+
+        CardModel? selectedCard = (await CardSelectCmd.FromCombatPile(
+                prefs: new CardSelectorPrefs(SelectionScreenPrompt, 1),
+                context: choiceContext,
+                pile: PileType.Discard.GetPile(Owner),
+                player: Owner,
+                filter: IsCyberDragonMonster))
+            .FirstOrDefault();
+        if (selectedCard != null) {
+            await CardCmd.AutoPlay(choiceContext, selectedCard, null);
+        }
+    }
+
+    private static bool IsCyberDragonMonster(CardModel card) {
+        return card is BaseMonsterCard monsterCard
+            && monsterCard.NameEquals(YgoMaterialNames.电子龙);
+    }
+
+    protected override void OnUpgrade() {
+        RemoveKeyword(CardKeyword.Exhaust);
+    }
+}
