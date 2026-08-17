c95571cb70e08950d18fe0e6329878560bfbe40e
Vitech <i@vitechliu.com>
2026-08-17 19:26:51 +0800

    feat: 效果发动限制

---
+++ Core/History/EffectEntry.cs
@@ -0,0 +1,39 @@
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Combat.History;
+using MegaCrit.Sts2.Core.Entities.Players;
+using VYgo.Scripts.Cards;
+
+namespace VYgo.Core.History;
+
+public sealed class EffectEntry(
+    Player player,
+    int cardId,
+    string sign,
+    ICombatState combatState,
+    CombatHistory history)
+    : CombatHistoryEntry(
+        player.Creature,
+        combatState.RoundNumber,
+        combatState.CurrentSide,
+        history,
+        combatState.Players), IYgoId {
+    public Player Player { get; } = player;
+
+    public int CardId { get; } = cardId;
+
+    public override string Description =>
+        $"{Player.Character.Id.Entry} use {CardId}'s effect: {sign}";
+}
+
+public static class EffectHistory {
+    public static void RecordUseEffect(
+        this CombatHistory history,
+        IYgoId card,
+        string sign,
+        ICombatState combatState,
+        Player player) {
+        history.Add(
+            combatState,
+            new EffectEntry(player, card.CardId, sign, combatState, history));
+    }
+}
